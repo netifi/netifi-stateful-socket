b9265e911074dac65bccbde3e9d2b9fe0ebceed4
Robert Roeser <rroeserr@gmail.com>
2019-05-20 13:14:42 -0700

    update to 1.6.3

--- service/src/main/java/com/netifi/quickstart/service/Main.java
+++ service/src/main/java/com/netifi/quickstart/service/Main.java
@@ -1,10 +1,7 @@
 package com.netifi.quickstart.service;
 
-import com.netifi.common.tags.Tags;
-import com.netifi.spring.core.BrokerClientTagSupplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
